9052816edd3f2205005cd92feef80d56af2287eb
b3ngineer <ben.harris.when@gmail.com>
2018-10-21 08:23:00 -0600

    Basic syntax for reducers

--- __test__/test.spec.js
+++ __test__/test.spec.js
@@ -1,71 +1,33 @@
 /* global describe test expect */
 /* eslint no-unused-expressions: 0 */
 
-import { type, reduce } from '../src/index'
-
-/*
- * import actions from "./actions"
-
-  const initState = {
-    deployments: {},
-    history: {},
-    loading: false
-  }
-
-  export default function reducer(state = initState, action) {
-    switch (action.type) {
-      case actions.DEPLOYMENT_LOAD:
-        return {
-          ...state,
-          loading: true,
-          deployments: Object.assign({}, state.deployments, {
-            [action.payload.name]: null
-          })
-        }
-      case actions.DEPLOYMENT_SUCCESS_RESULT:
-        return {
-          ...state,
-          loading: false,
-          deployments: Object.assign({}, state.deployments, {
-            [action.payload.name]: action.payload.result
-          })
-        }
-      case actions.HISTORY_LOAD:
-        return {
-          ...state,
-          loading: true,
-          history: Object.assign({}, state.history, {
-            [action.payload.host]: {
-              [action.payload.asset]: null
-            }
-          })
-        }
-      case actions.HISTORY_SUCCESS_RESULT:
-        return {
-          ...state,
-          loading: false,
-          history: Object.assign({}, state.history, {
-            [action.payload.host]: {
-              [action.payload.asset]: action.payload.result
-            }
-          })
-        }
-      default:
-        return state
-    }
-  }
- */
+import { action, reduce } from '../src/index'
 
 describe('terse-redux', () => {
-  test('type should be exposed', () => {
-    expect(typeof type).toEqual('function')
+  test('action should be exposed', () => {
+    expect(typeof action).toEqual('function')
   })
 
   test('reduce should be exposed', () => {
     expect(typeof reduce).toEqual('function')
   })
 
-  test('type should accept tag template and reduce function', () => {
-    const a = type`TEST_VALUE`
+  test('action should accept tag template parameter', () => {
+    const testAction = action`TEST_ACTION`
+    testAction.Δ = action => ({ something: action.something })
+    expect(testAction.name).toEqual('TEST_ACTION')
+  })
+
+  test('reduce should accept initial state and actions', () => {
+    const testAction = action`TEST_ACTION`
+    testAction.Δ = () => ({ foo: 'baz' })
+    const reducer = reduce({ foo: 'bar' }, [testAction])
+    expect(typeof reducer).toEqual('function')
+  })
+
+  test('action types should expose bindable action builder', () => {
+    const testAction = action`TEST_ACTION`
+    testAction.Δ = () => ({ foo: 'baz' })
+    expect(typeof testAction.builder).toEqual('function')
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,16 +1,55 @@
-class ReduxTypeAction {
+import _ from 'lodash' // @types/lodash.clonedeep
+
+const { cloneDeep } = _
+
+class Action {
   constructor (name) {
     this.name = Array.isArray(name) ? name[0] : name
+    if (this.name) {
+      this.name = this.name.toUpperCase()
+    }
+    this._delta = action => {}
+
+    this._builder = () => ({
+      type: this.name,
+      payload: {
+        ...arguments
+      }
+    })
   }
 
-  newup (state) {}
+  set delta (factory) {
+    this._delta = factory
+  }
+
+  set Δ (factory) {
+    this._delta = factory
+  }
+
+  get builder () {
+    return this._builder
+  }
+
+  merge (state, action) {
+    return Object.assign(cloneDeep(state), this._delta(action))
+  }
+
+  matches (requestedAction) {
+    return requestedAction === this.name
+  }
 }
 
-export const type = name => new ReduxTypeAction(name)
+class AttemptAction extends Action {
+  matches (requestedAction) {
+    return requestedAction === this.name || this.name + '_SUCCESS' || this.name + '_ERROR'
+  }
+}
+
+export const action = name => new Action(name)
+
+export const attempt = name => new AttemptAction(name)
 
 export const reduce = (initialState = {}, handlers = []) => (state, action) =>
   handlers
-    .filter(handler => handler.name === action.type)
-    .reduce((base, handler) => {
-      return Object.assign({}, base)
-    }, state || initialState)
+    .filter(handler => handler.matches(action.type))
+    .reduce((_state, handler) => handler.merge(_state, action), state || initialState)
